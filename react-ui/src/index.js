4b40207038ef05a6f5c44a926a309ef86d1b1f00
Anni Kynsilehto <anni.kynsilehto@gmail.com>
2021-08-03 20:24:03 +0300

    Begin setting up Apollo Client

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -3,11 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:3001',
+  }),
+});
+
+const query = gql`
+  query {
+    totalVaccinationCount
+  }
+`;
+
+client.query({ query }).then((response) => {
+  console.log(response.data);
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
